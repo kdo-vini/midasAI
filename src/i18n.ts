3e482262206d977f872fc4885c76e68d7a923280
Vinicius <kdo.vinI@gmail.com>
2026-02-25 15:21:35 -0300

    refactor(i18n): remove English translations, keep Portuguese only

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,31 +1,20 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import LanguageDetector from 'i18next-browser-languagedetector';
 
-import translationEN from './locales/en/translation.json';
 import translationPT from './locales/pt/translation.json';
 
 const resources = {
-    en: {
-        translation: translationEN
-    },
     pt: {
         translation: translationPT
     }
 };
 
 i18n
-    .use(LanguageDetector)
     .use(initReactI18next)
     .init({
         resources,
+        lng: 'pt',
         fallbackLng: 'pt',
-        supportedLngs: ['pt', 'en'],
-        nonExplicitSupportedLngs: true,
-        detection: {
-            order: ['querystring', 'localStorage', 'navigator'],
-            caches: ['localStorage'],
-        },
         debug: true,
         interpolation: {
             escapeValue: false, // not needed for react as it escapes by default
